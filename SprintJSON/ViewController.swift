1048451ef85fd2201a208bf0fb62a455a591cba0
Ian Shoenrock <ian.shoenrock@anthem.com>
2019-03-07 15:34:19 -0500

    Initial Commit

---
+++ SprintJSON/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SprintJSON
+//
+//  Created by Ian Schoenrock on 3/7/19.
+//  Copyright © 2019 Ian Schoenrock. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
